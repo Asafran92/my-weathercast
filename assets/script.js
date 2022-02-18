e5835cd43adab38fedc2d032613e068297960b88
unknown <avrumiesafranovitz@gmail.com>
2022-02-17 21:15:49 -0500

    add logic which displays current weather stats for an input

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,43 @@
 var apiKey = "2e87b116b64f5074218c9d8fc477d9b1";
 
 function citySearch() {
-  //var cityName = document.getElementById("cityName").value;
-  var cityName = "New York";
+  var cityName = document.getElementById("cityName").value;
   fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=Atlanta&limit=1&appid=${apiKey}`
+    `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`
   )
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      console.log(data[0].lat, data[0].lon);
       var latitude = data[0].lat;
       var longitude = data[0].lon;
+      console.log(data);
       fetch(
-        `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}&exclude=minutely,alerts`
       )
         .then((response) => {
           return response.json();
         })
         .then((data) => {
-          console.log(data);
+          displayData(data);
         });
     })
     .catch((error) => {
       console.log(error);
     });
   //console.log(response)
+}
+
+function displayData(data) {
+  console.log(data);
+  document.getElementById("current-temp").innerHTML = `${data.current.temp}`;
+  document.getElementById(
+    "current-wind"
+  ).innerHTML = `${data.current.wind_speed}`;
+  document.getElementById(
+    "current-humidity"
+  ).innerHTML = `${data.current.humidity}`;
+  document.getElementById("current-uvi").innerHTML = `${data.current.uvi}`;
 }
 
 document.getElementById("search-btn").addEventListener("click", citySearch);
