b684f6095e6876d1fbc60ae7ff545522a3d9d605
unknown <avrumiesafranovitz@gmail.com>
2022-04-02 22:20:52 -0400

    added cards and weather logic

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var apiKey = "2e87b116b64f5074218c9d8fc477d9b1";
-
+var searchedName;
 function citySearch() {
   var cityName = document.getElementById("city-name").value;
   fetch(
@@ -11,6 +11,7 @@ function citySearch() {
     .then((data) => {
       var latitude = data[0].lat;
       var longitude = data[0].lon;
+      searchedName = data[0].name;
       console.log(data);
       fetch(
         `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}&exclude=minutely,alerts`
@@ -30,14 +31,37 @@ function citySearch() {
 
 function displayData(data) {
   console.log(data);
-  document.getElementById("current-temp").innerHTML = `${data.current.temp}`;
+  document.getElementById("currentCity").innerText = searchedName;
+  document.getElementById(
+    "current-temp"
+  ).innerText = `Temp: ${data.current.temp}`;
   document.getElementById(
     "current-wind"
-  ).innerHTML = `${data.current.wind_speed}`;
+  ).innerText = `Wind: ${data.current.wind_speed}`;
   document.getElementById(
     "current-humidity"
-  ).innerHTML = `${data.current.humidity}`;
-  document.getElementById("current-uvi").innerHTML = `${data.current.uvi}`;
+  ).innerText = `Humidity: ${data.current.humidity}`;
+  document.getElementById(
+    "current-uvi"
+  ).innerText = `UVI Index: ${data.current.uvi}`;
+  document.getElementById("forecast").innerHTML = "";
+
+  for (let i = 1; i <= 5; i++) {
+    var card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `<h4>Date</h4>
+    <img src="https://via.placeholder.com/35x35" />
+    <p>
+        Temp: ${data.daily[i].temp.day}
+    </p>
+    <p>
+        Wind: ${data.daily[i].wind_speed}
+    </p>
+    <p>
+        Humidity: ${data.daily[i].humidity}
+    </p>`;
+    document.getElementById("forecast").append(card);
+  }
 }
 
 document.getElementById("search-btn").addEventListener("click", citySearch);
