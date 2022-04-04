30dfe56d025c909bc50e7a4100d585e1f2e79e37
unknown <avrumiesafranovitz@gmail.com>
2022-04-03 21:23:13 -0400

    added dates and local storage

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 var apiKey = "2e87b116b64f5074218c9d8fc477d9b1";
 var searchedName;
-function citySearch() {
-  var cityName = document.getElementById("city-name").value;
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+function citySearch(cityName) {
   fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`
   )
@@ -21,6 +21,14 @@ function citySearch() {
         })
         .then((data) => {
           displayData(data);
+          if (!searchHistory.includes(searchedName)) {
+            searchHistory.push(searchedName);
+            localStorage.setItem(
+              "searchHistory",
+              JSON.stringify(searchHistory)
+            );
+            renderHistory();
+          }
         });
     })
     .catch((error) => {
@@ -28,10 +36,27 @@ function citySearch() {
     });
   //console.log(response)
 }
-
+function renderHistory() {
+  document.getElementById("searchHistory").innerHTML = "";
+  searchHistory.forEach((cityName) => {
+    var listItem = document.createElement("li");
+    listItem.innerText = cityName;
+    listItem.addEventListener("click", function () {
+      citySearch(cityName);
+    });
+    document.getElementById("searchHistory").append(listItem);
+  });
+}
 function displayData(data) {
   console.log(data);
-  document.getElementById("currentCity").innerText = searchedName;
+  document.getElementById(
+    "currentCity"
+  ).innerText = `${searchedName} ${new Date(
+    data.current.dt * 1000
+  ).toDateString()}`;
+  document.getElementById(
+    "currentIcon"
+  ).src = `http://openweathermap.org/img/wn/${data.current.weather[0].icon}.png`;
   document.getElementById(
     "current-temp"
   ).innerText = `Temp: ${data.current.temp}`;
@@ -49,8 +74,12 @@ function displayData(data) {
   for (let i = 1; i <= 5; i++) {
     var card = document.createElement("div");
     card.classList.add("card");
-    card.innerHTML = `<h4>Date</h4>
-    <img src="https://via.placeholder.com/35x35" />
+    card.innerHTML = `<h4>${new Date(
+      data.daily[i].dt * 1000
+    ).toDateString()}</h4>
+    <div> <img src="http://openweathermap.org/img/wn/${
+      data.daily[i].weather[0].icon
+    }.png" /> </div>
     <p>
         Temp: ${data.daily[i].temp.day}
     </p>
@@ -63,5 +92,7 @@ function displayData(data) {
     document.getElementById("forecast").append(card);
   }
 }
-
-document.getElementById("search-btn").addEventListener("click", citySearch);
+renderHistory();
+document.getElementById("search-btn").addEventListener("click", function () {
+  citySearch(document.getElementById("city-name").value);
+});
